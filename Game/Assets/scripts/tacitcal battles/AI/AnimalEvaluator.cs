1c1f7996aac9f99386a2d4c98b31479daff94561
Shachar <nihohit@gmail.com>
2014-04-20 16:56:16 +0300

    Initial AI implementation. Untested.

---
+++ Game/Assets/scripts/tacitcal battles/AI/AnimalEvaluator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+/**there's a hidden assumption here that no AI of this type will have
+ * a system which affects empty hexes. Since this is supposed to be 
+ * only for the simplest of AIs, this assumption should hold. */
+public class AnimalEvaluator : IActionEvaluator 
+{
+    private IEntityEvaluator m_entityEvaluator;
+    private IEnumerable<ActiveEntity> m_controlledEntities;
+
+    #region constructor
+
+    public AnimalEvaluator(IEnumerable<ActiveEntity> controlledEntities, IEntityEvaluator entityEvaluator)
+    {
+        m_entityEvaluator = entityEvaluator;
+        m_controlledEntities = controlledEntities;
+    }
+
+    #endregion
+
+    #region IActionEvaluator implementation
+
+    /**evaluates a system action based on the importance of its target,
+     * and movement commands based on nearness to potential targets. 
+     * If no potential targets are in sight, randomly roam. */
+    public IEnumerable<EvaluatedAction> EvaluateActions(ActiveEntity actingEntity)
+    {
+        var potentialTargets = actingEntity.SeenHexes.Select(hex => hex.Content).
+            OrderBy(ent => m_entityEvaluator.EvaluateValue(ent)).
+                Where(ent => m_entityEvaluator.EvaluateValue(ent) > 0);
+        var movingEntity = actingEntity as MovingEntity;
+        foreach (var action in actingEntity.Actions)
+        {
+            var evaluatedAction = new EvaluatedAction();
+            evaluatedAction.Action = action;
+            var systemAction = action as OperateSystemAction;
+            var movementAction = action as MovementAction;
+            if (systemAction != null)
+            {
+                var target = systemAction.TargetedHex.Content;
+                evaluatedAction.EvaluatedPriority = m_entityEvaluator.EvaluateValue(target);
+                evaluatedAction.NecessaryConditions = () =>
+                {
+                    return !actingEntity.Destroyed() && !target.Destroyed();
+                };
+                evaluatedAction.AchievedGoal = () => 
+                {
+                    return target.Loyalty == actingEntity.Loyalty || target.Destroyed();
+                };
+            } else if (movementAction != null)
+            {
+                var value = 0.0;
+                foreach(var target in potentialTargets)
+                {
+                    value +=  m_entityEvaluator.EvaluateValue(target) / AStar.FindPathCost(
+                        movementAction.TargetedHex, target.Hex, new AStarConfiguration(movingEntity.MovementMethod, (hex) => 0));
+
+                }
+                evaluatedAction.EvaluatedPriority = value;
+                evaluatedAction.NecessaryConditions = () =>
+                {
+                    return !actingEntity.Destroyed() && movementAction.TargetedHex.Content == null;
+                };
+                evaluatedAction.AchievedGoal = () => 
+                {
+                    return movementAction.TargetedHex.Content == actingEntity;
+                };
+            }
+
+            yield return evaluatedAction;
+        }
+    }
+    #endregion
+}
