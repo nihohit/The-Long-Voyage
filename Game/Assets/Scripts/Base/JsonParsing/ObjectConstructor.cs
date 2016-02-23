cf3c9ac3410311513fcb6f2376f0e44bc49b36df
Shachar <nihohit@gmail.com>
2016-02-23 21:57:10 +0200

    Object constructor will now display name of object and parameter when throwing error. Also can handle receiving null for enumerable.

--- Game/Assets/Scripts/Base/JsonParsing/ObjectConstructor.cs
+++ Game/Assets/Scripts/Base/JsonParsing/ObjectConstructor.cs
@@ -1,155 +1,176 @@
 ﻿namespace Assets.Scripts.Base.JsonParsing
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
 
-    #region ConstructorBasedJSONParser
+	using UnityEngine;
 
-    public static class ObjectConstructor
-    {
-        #region public methods
+	#region ConstructorBasedJSONParser
 
-        public static T ParseObject<T>(object parsedObject)
-        {
-            var type = typeof(T);
-            if (type.IsPrimitive)
-            {
-                parsedObject = Convert.ChangeType(parsedObject, type);
-            }
-            else if (type.IsEnum)
-            {
-                parsedObject = Enum.Parse(type, parsedObject.ToString());
-            }
-            else if (type == typeof(string))
-            {
-            }
-            else
-            {
-                parsedObject = ParseObject(parsedObject, typeof(T));
-            }
+	public static class ObjectConstructor
+	{
+		#region public methods
 
-            return (T)parsedObject;
-        }
+		public static T ParseObject<T>(object parsedObject)
+		{
+			var type = typeof(T);
+			if (type.IsPrimitive)
+			{
+				parsedObject = Convert.ChangeType(parsedObject, type);
+			}
+			else if (type.IsEnum)
+			{
+				parsedObject = Enum.Parse(type, parsedObject.ToString());
+			}
+			else if (type == typeof(string))
+			{
+			}
+			else
+			{
+				try
+				{
+					parsedObject = ParseObject(parsedObject, type);
+				}
+				catch(AssertedException ex)
+				{
+					throw new AssertedException(" in type {0}".FormatWith(type.Name), ex);
+				}
+			}
 
-        private static object ParseObject(object parsedObject, Type type)
-        {
-            if (parsedObject == null)
-            {
-                return null;
-            }
+			return (T)parsedObject;
+		}
 
-            ConstructorInfo constructor;
+		private static object ParseObject(object parsedObject, Type type)
+		{
+			if (parsedObject == null)
+			{
+				return null;
+			}
 
-            var markedConstructors =
-                type.GetConstructors().Where(ctor =>
-                    ctor.GetCustomAttributes(false).Any(attr =>
-                        attr.GetType() == typeof(ChosenConstructorForParsing))).ToList();
+			ConstructorInfo constructor;
 
-            Assert.EqualOrLesser(markedConstructors.Count(), 1, "More than a single marked constructor");
+			var markedConstructors =
+				type.GetConstructors().Where(ctor =>
+					ctor.GetCustomAttributes(false).Any(attr =>
+						attr.GetType() == typeof(ChosenConstructorForParsing))).ToList();
 
-            if (markedConstructors.None())
-            {
-                Assert.AreEqual(type.GetConstructors().Count(), 1, "More than a single construcor");
-                constructor = type.GetConstructors().First();
-            }
-            else
-            {
-                constructor = markedConstructors.First();
-            }
+			Assert.EqualOrLesser(markedConstructors.Count(), 1, "More than a single marked constructor");
 
-            var parameters = new object[constructor.GetParameters().Count()];
-            var objectAsDictionary = parsedObject.SafeCast<IDictionary<string, object>>("parsedObject");
+			if (markedConstructors.None())
+			{
+				Assert.AreEqual(type.GetConstructors().Count(), 1, "More than a single constructor");
+				constructor = type.GetConstructors().First();
+			}
+			else
+			{
+				constructor = markedConstructors.First();
+			}
 
-            objectAsDictionary = new Dictionary<string, object>(
-                objectAsDictionary,
-                StringComparer.InvariantCultureIgnoreCase);
-            var index = 0;
+			var parameters = new List<object>();
+			var objectAsDictionary = parsedObject.SafeCast<IDictionary<string, object>>("parsedObject");
 
-            foreach (var param in constructor.GetParameters())
-            {
-                object obj;
+			objectAsDictionary = new Dictionary<string, object>(
+				objectAsDictionary,
+				StringComparer.InvariantCultureIgnoreCase);
 
-                if (!objectAsDictionary.TryGetValue(param.Name, out obj))
-                {
-                    if (param.IsOptional)
-                    {
-                        obj = param.DefaultValue;
-                    }
-                    else
-                    {
-                        throw new ArgumentException("Parameter {0} is missing in {1}".FormatWith(param.Name, objectAsDictionary));
-                    }
-                }
+			foreach (var param in constructor.GetParameters())
+			{
+				try
+				{
+					parameters.Add(parseParameter(param, objectAsDictionary));
+				}
+				catch(AssertedException ex)
+				{
+					throw new AssertedException(" for parameter {0}".FormatWith(param.Name), ex);
+				}
+				catch(TargetInvocationException ex)
+				{
+					throw new AssertedException(" for parameter {0}".FormatWith(param.Name), (AssertedException)ex.InnerException);
+				}
+			}
 
-                if (param.ParameterType.IsPrimitive)
-                {
-                    parameters[index] = Convert.ChangeType(obj, param.ParameterType);
-                }
-                else if (param.ParameterType.IsEnum)
-                {
-                    parameters[index] = Enum.Parse(param.ParameterType, obj.ToString());
-                }
-                else if (param.ParameterType == typeof(string))
-                {
-                    parameters[index] = obj;
-                }
-                else if (param.ParameterType.IsGenericType && param.ParameterType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
-                {
-                    // it's an enumerable, we'll call find the relevant generic version of ParseEnumerable and call it.
-                    Type underlyingType = param.ParameterType.GetGenericArguments()[0];
-                    var method = typeof(ObjectConstructor).GetMethod(
-                        "ParseEnumerable",
-                        BindingFlags.NonPublic | BindingFlags.Static);
-                    method = method.MakeGenericMethod(underlyingType);
-                    parameters[index] = method.Invoke(null, new[] { obj });
-                }
-                /*
-                else if (param.ParameterType.GetInterfaces().Any(i => i == typeof(IIdentifiable<string>)) && obj is string)
-                {
-                    // TODO - do we need to handle a generic IIdentifiable?
-                    // it's a type that implement IIdentifiable
-                    var configurationStorageType = typeof(ConfigurationStorage<>);
-                    configurationStorageType = configurationStorageType.MakeGenericType(param.ParameterType);
-                    var instanceProperty = configurationStorageType.GetProperty(
-                        "Instance",
-                        BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Static);
-                    var instance = instanceProperty.GetGetMethod().Invoke(null, null);
-                    var getConfigurationMethod = configurationStorageType.GetMethod("GetConfiguration");
+			return constructor.Invoke(parameters.ToArray());
+		}
 
-                    parameters[index] = getConfigurationMethod.Invoke(instance, new[] { obj });
-                }
-                 */
-                else
-                {
-                    parameters[index] = ParseObject(obj, param.ParameterType);
-                }
+		private static object parseParameter(ParameterInfo param, IDictionary<string, object> objectAsDictionary)
+		{
+			object obj;
 
-                index++;
-            }
+			if (!objectAsDictionary.TryGetValue(param.Name, out obj))
+			{
+				Assert.AssertConditionMet(param.IsOptional, "Missing parameter isn't optional");
+				obj = param.DefaultValue;
+			}
 
-            return constructor.Invoke(parameters);
-        }
+			if (param.ParameterType.IsPrimitive)
+			{
+				return Convert.ChangeType(obj, param.ParameterType);
+			}
+			else if (param.ParameterType.IsEnum)
+			{
+				return Enum.Parse(param.ParameterType, obj.ToString());
+			}
+			else if (param.ParameterType == typeof(string))
+			{
+				return obj;
+			}
+			else if (param.ParameterType.IsGenericType && param.ParameterType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+			{
+				// it's an enumerable, we'll call find the relevant generic version of ParseEnumerable and call it.
+				Type underlyingType = param.ParameterType.GetGenericArguments()[0];
+				var method = typeof(ObjectConstructor).GetMethod(
+					"ParseEnumerable",
+					BindingFlags.NonPublic | BindingFlags.Static);
+				method = method.MakeGenericMethod(underlyingType);
+				return method.Invoke(null, new[] { obj });
+			}
+			/*
+			else if (param.ParameterType.GetInterfaces().Any(i => i == typeof(IIdentifiable<string>)) && obj is string)
+			{
+				// TODO - do we need to handle a generic IIdentifiable?
+				// it's a type that implement IIdentifiable
+				var configurationStorageType = typeof(ConfigurationStorage<>);
+				configurationStorageType = configurationStorageType.MakeGenericType(param.ParameterType);
+				var instanceProperty = configurationStorageType.GetProperty(
+					"Instance",
+					BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Static);
+				var instance = instanceProperty.GetGetMethod().Invoke(null, null);
+				var getConfigurationMethod = configurationStorageType.GetMethod("GetConfiguration");
 
-        private static IEnumerable<TValue> ParseEnumerable<TValue>(object enumerable)
-        {
-            var objectAsEnumerable = enumerable.SafeCast<IEnumerable<object>>("enumerable");
+				parameters[index] = getConfigurationMethod.Invoke(instance, new[] { obj });
+			}
+			 */
+			else
+			{
+				return ParseObject(obj, param.ParameterType);
+			}
+		}
 
-            return objectAsEnumerable.Select(item => ParseObject<TValue>(item));
-        }
+		private static IEnumerable<TValue> ParseEnumerable<TValue>(object enumerable)
+		{
+			if(enumerable == null)
+			{
+				return Enumerable.Empty<TValue>();
+			}
 
-        #endregion public methods
-    }
+			var objectAsEnumerable = enumerable.SafeCast<IEnumerable<object>>("enumerable");
 
-    #endregion ConstructorBasedJSONParser
+			return objectAsEnumerable.Select(item => ParseObject<TValue>(item));
+		}
 
-    #region constrcutor attrbute
+		#endregion public methods
+	}
 
-    [AttributeUsage(AttributeTargets.Constructor)]
-    public class ChosenConstructorForParsing : Attribute
-    {
-    }
+	#endregion ConstructorBasedJSONParser
 
-    #endregion constrcutor attrbute
+	#region constrcutor attribute
+
+	[AttributeUsage(AttributeTargets.Constructor)]
+	public class ChosenConstructorForParsing : Attribute
+	{
+	}
+
+	#endregion constrcutor attrbute
 }
